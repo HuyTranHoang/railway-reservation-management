8f6ce14b1e4ff141a457545ee0741b67b2240de9
Huy Tran Hoang <nhokpio95@gmail.com>
2023-12-30 10:26:59 +0700

    Remove errors folder

--- WebApi/Middleware/ExceptionMiddleware.cs
+++ WebApi/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net;
 using System.Text.Json;
-using WebApi.Errors;
+using WebApi.Exceptions;
 
 namespace WebApi.Middleware;
 
@@ -26,13 +26,13 @@ public class ExceptionMiddleware
         {
             _logger.LogError(ex, ex.Message);
             context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
 
             var response = _env.IsDevelopment()
-                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+                ? new ErrorResponse(context.Response.StatusCode, ex.Message + ex.StackTrace)
+                : new ErrorResponse(context.Response.StatusCode);
 
-            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var options = new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
             var json = JsonSerializer.Serialize(response, options);
 
