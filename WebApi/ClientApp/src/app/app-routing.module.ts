cec217700d3195504d32f6172feb00d4e36ebf36
Huy Trần <nhokpio95@gmail.com>
2024-01-02 21:38:53 +0700

    Fix css navbar, add signup page

--- WebApi/ClientApp/src/app/app-routing.module.ts
+++ WebApi/ClientApp/src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { TestErrorComponent } from './core/test-error/test-error.component'
 import { NotFoundComponent } from './core/not-found/not-found.component'
 import { ServerErrorComponent } from './core/server-error/server-error.component'
-import { BookingTrainComponent } from './booking-train/booking-train.component';
+import { BookingTrainComponent } from './booking-train/booking-train.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(mod => mod.AuthModule)},
   {path: 'test-error', component: TestErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
   {path: 'server-error', component: ServerErrorComponent},
-  {path: 'booking-train', component: BookingTrainComponent},
-];
+  {path: 'booking-train', component: BookingTrainComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
