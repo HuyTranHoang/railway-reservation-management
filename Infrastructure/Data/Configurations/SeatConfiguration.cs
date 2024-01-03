e55904840c09aa4e9818b8ac1f8613fe3b92b60e
Huy Trần <nhokpio95@gmail.com>
2024-01-03 12:57:06 +0700

    Add Seat configuration for DeleteBehavior

---
+++ Infrastructure/Data/Configurations/SeatConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class SeatConfiguration : IEntityTypeConfiguration<Seat>
+{
+    public void Configure(EntityTypeBuilder<Seat> builder)
+    {
+        builder.HasQueryFilter(e => !e.IsDeleted);
+
+        builder
+            .HasOne(s => s.SeatType)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(s => s.Compartment)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Infrastructure/Data/Migrations/20240103055625_SeatConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class SeatConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seats_Compartments_CompartmentId",
+                table: "Seats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seats_SeatTypes_SeatTypeId",
+                table: "Seats");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seats_Compartments_CompartmentId",
+                table: "Seats",
+                column: "CompartmentId",
+                principalTable: "Compartments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seats_SeatTypes_SeatTypeId",
+                table: "Seats",
+                column: "SeatTypeId",
+                principalTable: "SeatTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seats_Compartments_CompartmentId",
+                table: "Seats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seats_SeatTypes_SeatTypeId",
+                table: "Seats");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seats_Compartments_CompartmentId",
+                table: "Seats",
+                column: "CompartmentId",
+                principalTable: "Compartments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seats_SeatTypes_SeatTypeId",
+                table: "Seats",
+                column: "SeatTypeId",
+                principalTable: "SeatTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
