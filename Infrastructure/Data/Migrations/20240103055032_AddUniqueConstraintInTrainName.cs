031f4a21a71390a1f79fa05c55c3d602833b104e
Huy Trần <nhokpio95@gmail.com>
2024-01-03 12:52:08 +0700

    Add index and constraint for train name

---
+++ Infrastructure/Data/Migrations/20240103055032_AddUniqueConstraintInTrainName.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddUniqueConstraintInTrainName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Cancellation",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trains_Name",
+                table: "Trains",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Trains_Name",
+                table: "Trains");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "Cancellation",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450);
+        }
+    }
+}
