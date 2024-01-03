bc308def2e6b365537cf75b09a8f42bebeeaea4a
TruongThaiBao <infocheck0808@gmail.com>
2024-01-04 06:42:08 +0700

    Add RoundTrip and Migrations

---
+++ Infrastructure/Data/Configurations/RoundTripConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class RoundTripConfiguration : IEntityTypeConfiguration<RoundTrip>
+{
+    public void Configure(EntityTypeBuilder<RoundTrip> builder)
+    {
+        builder.HasQueryFilter(e => !e.IsDeleted);
+
+        builder
+            .HasOne(t => t.TrainCompany)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
