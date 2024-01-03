5e66d4b0c08cb2238df82c215ef492bbbde20493
TruongThaiBao <infocheck0808@gmail.com>
2024-01-04 06:09:00 +0700

    Create CarriageType and Migrations

---
+++ Domain/Entities/CarriageType.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class CarriageType : BaseEntity
+    {
+        public int Id { get; set; }
+        
+        [Required] [StringLength(100)] public string Name { get; set; }
+
+        public double ServiceCharge { get; set; }
+
+        [Required] public string Status { get; set; }
+    }
+}
---
+++ Infrastructure/Data/Configurations/CarriageTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class CarriageTypeConfiguration : IEntityTypeConfiguration<CarriageType>
+{
+    public void Configure(EntityTypeBuilder<CarriageType> builder)
+    {
+        builder.HasQueryFilter(e => !e.IsDeleted);
+    }
+}
