671b90d484442e404df15bad973ac446cf3ba935
Huy Trần <nhokpio95@gmail.com>
2024-01-04 12:40:11 +0700

    Add migrations for unique index of compartment

---
+++ Infrastructure/Data/Migrations/20240104053743_AddUniqueConstraintInCompartment.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddUniqueConstraintInCompartment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Compartments_Name",
+                table: "Compartments",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Compartments_Name",
+                table: "Compartments");
+        }
+    }
+}
