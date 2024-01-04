1f369e1296782d8eb0642f8d438df13db8cdce9c
TruongThaiBao <infocheck0808@gmail.com>
2024-01-05 05:08:02 +0700

    Create TrainStation Service

---
+++ Application/Common/Models/TrainStationDto.cs
@@ -0,0 +1,11 @@
+namespace Application.Common.Models
+{
+    public class TrainStationDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int CoordinateValue { get; set; }
+        public string Status { get; set; }
+    }
+}
