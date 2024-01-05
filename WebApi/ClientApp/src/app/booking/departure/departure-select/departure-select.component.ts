dad4f650b006109d64aacc2fcc4b367e2e82233b
Huy Trần <nhokpio95@gmail.com>
2024-01-06 01:58:37 +0700

    Add table for display train

---
+++ WebApi/ClientApp/src/app/booking/departure/departure-select/departure-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-departure-select',
+  templateUrl: './departure-select.component.html',
+  styleUrls: ['./departure-select.component.scss']
+})
+export class DepartureSelectComponent {
+
+}
