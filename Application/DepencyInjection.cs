b82f979d059178cafd96ed2de54612aa9c5f7389
Huy Trần <nhokpio95@gmail.com>
2024-01-08 20:45:34 +0700

    Update DepencyInjection.cs

--- Application/DepencyInjection.cs
+++ Application/DepencyInjection.cs
@@ -8,7 +8,13 @@ public static class DepencyInjection
 {
   public static IServiceCollection AddApplication(this IServiceCollection services)
   {
+    //Email Service
+    services.AddScoped<EmailService>();
+
+    //AutoMapper Service
     services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+    //Persistence Services
     services.AddScoped<ICarriageService, CarriageService>();
     services.AddScoped<ICarriageTypeService, CarriageTypeService>();
     services.AddScoped<ICompartmentService, CompartmentService>();
@@ -22,6 +28,7 @@ public static class DepencyInjection
     services.AddScoped<IRoundTripService, RoundTripService>();
     services.AddScoped<ITicketService, TicketService>();
     services.AddScoped<IScheduleService, ScheduleService>();
+    services.AddScoped<IDistanceFareService, DistanceFareService>();
     return services;
   }
 }
