339dcb852fc65676960301df2233d51822a3d8a4
Huy Trần <nhokpio95@gmail.com>
2024-01-14 15:11:41 +0700

    Remove train auto complete

--- WebApi/AdminApp/src/app/managements/train-and-carriage/train/add-train/add-train.component.ts
+++ WebApi/AdminApp/src/app/managements/train-and-carriage/train/add-train/add-train.component.ts
@@ -1,11 +1,9 @@
 import {TrainService} from '../train.service';
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {NbGlobalPhysicalPosition, NbToastrService} from '@nebular/theme';
 import {TrainCompanyService} from '../../../railway/train-company/train-company.service';
 import {TrainCompany} from '../../../../@models/trainCompany';
-import {map} from 'rxjs/operators';
-import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'ngx-add-train',
@@ -14,11 +12,7 @@ import {Observable, of} from 'rxjs';
 })
 export class AddTrainComponent implements OnInit {
 
-  options: TrainCompany[];
-  filteredOptions$: Observable<TrainCompany[]>;
-  isCompanySelected: boolean = false;
-
-  @ViewChild('autoInput') input: { nativeElement: { value: string; }; };
+  trainCompanies: TrainCompany[] = [];
 
   trainForm: FormGroup = this.fb.group({});
   isSubmitted: boolean = false;
@@ -31,8 +25,8 @@ export class AddTrainComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.initForm();
     this.loadAllTrainCompany();
+    this.initForm();
   }
 
   initForm() {
@@ -46,11 +40,6 @@ export class AddTrainComponent implements OnInit {
   onSubmit() {
     this.isSubmitted = true;
     this.errorMessages = [];
-
-    if (!this.isCompanySelected && this.input.nativeElement.value !== '') {
-      this.errorMessages.push('Please select a valid train company.');
-      return;
-    }
 
     if (this.trainForm.valid) {
       this.trainService.addTrain(this.trainForm.value).subscribe({
@@ -69,43 +58,11 @@ export class AddTrainComponent implements OnInit {
   }
 
   loadAllTrainCompany() {
-    this.trainCompanyService.getAllTrainCompanyNoPaging().subscribe(res => {
-      this.options = res;
-      this.filteredOptions$ = of(this.options);
+    this.trainCompanyService.getAllTrainCompanyNoPaging().subscribe({
+      next: (res) => {
+        this.trainCompanies = res;
+      },
     });
-  }
-
-  private filter(value: string): TrainCompany[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
-  }
-
-  getFilteredOptions(value: string): Observable<TrainCompany[]> {
-    return of(value).pipe(
-      map(filterString => this.filter(filterString)),
-    );
-  }
-
-  onChange() {
-    this.isCompanySelected = false;
-    this.errorMessages = [];
-    this.filteredOptions$ = this.getFilteredOptions(this.input.nativeElement.value);
-  }
-
-  onBlur() {
-    const inputValue = this.input.nativeElement.value;
-    const matchingCompany = this.options.find(company => company.name === inputValue);
-
-    if (!matchingCompany) {
-      this.trainForm.patchValue({ trainCompanyId: null });
-      this.isCompanySelected = false;
-    }
-  }
-
-  onSelectionChange(trainCompany: TrainCompany) {
-    this.isCompanySelected = true;
-    this.trainForm.patchValue({ trainCompanyId: trainCompany.id });
-    this.input.nativeElement.value = trainCompany.name;
   }
 
   private showToast(type: string, title: string, body: string) {
