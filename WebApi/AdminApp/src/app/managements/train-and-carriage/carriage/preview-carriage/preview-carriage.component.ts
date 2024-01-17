4c626016a92f5c39c36d78549c43934fc191160d
Huy Trần <nhokpio95@gmail.com>
2024-01-18 03:51:17 +0700

    Implement ghế theo chiều dọc

--- WebApi/AdminApp/src/app/managements/train-and-carriage/carriage/preview-carriage/preview-carriage.component.ts
+++ WebApi/AdminApp/src/app/managements/train-and-carriage/carriage/preview-carriage/preview-carriage.component.ts
@@ -1,17 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-preview-carriage',
   templateUrl: './preview-carriage.component.html',
   styleUrls: ['./preview-carriage.component.scss'],
 })
-export class PreviewCarriageComponent {
-  // Tạo ghế
-  seats: any[] = [];
+export class PreviewCarriageComponent implements OnInit {
+
+  seatRows: number[][] = [];
 
   constructor() {
-    for (let i = 1; i <= 64; i++) {
-      this.seats.push(i);
-    }
   }
+
+  ngOnInit(): void {
+    this.seatRows = this.generateSeatRows();
+  }
+
+  generateSeatRows(): number[][] {
+    const seatRows = [];
+    const rows = 4; // Số hàng ghế trong mỗi khối (đã được chia đôi)
+    const cols = 8; // Số lượng ghế trên mỗi hàng
+    const half = 32; // Số ghế trong một nửa của toàn bộ ghế
+
+    // Xử lý cho nửa đầu của các ghế
+    for (let row = 0; row < rows; row++) {
+      seatRows[row] = [];
+      for (let col = 0; col < cols; col++) {
+        seatRows[row][col] = row + 1 + col * rows;
+      }
+    }
+
+    // Xử lý cho nửa sau của các ghế
+    for (let row = 0; row < rows; row++) {
+      seatRows[row + rows] = []; // Bắt đầu từ hàng thứ 5 (index 4)
+      for (let col = 0; col < cols; col++) {
+        seatRows[row + rows][col] = half + row + 1 + col * rows;
+      }
+    }
+
+    return seatRows;
+  }
+
 }
