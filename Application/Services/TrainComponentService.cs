b5c160a13ab051f722b950d70340949efcd264e4
TruongThaiBao <infocheck0808@gmail.com>
2024-01-18 10:36:43 +0700

    Khi add Carriage, tự động khởi tạo Compartment & Seat tương ứng

--- Application/Services/TrainComponentService.cs
+++ Application/Services/TrainComponentService.cs
@@ -42,13 +42,26 @@ namespace Application.Services
                 {
                     Name = $"Conpartment-{i+1}",
                     CarriageId = carriageId,
-                    NumberOfSeats = 10,
+                    NumberOfSeats = 6, //Nghiệp vu mặc định NumberOfSeats dựa theo Compartment
                 };
 
                 await _compartmentRepository.Add(compartment);
+                await _unitOfWork.SaveChangesAsync();
+
+
+                //Loop thêm 1 lần nữa để add Seat dựa trên NumberOfSeats trong Compartment
+                for (int j = 0; j < compartment.NumberOfSeats; j++)
+                {
+                    var seat = new Seat
+                    {
+                        Name = $"{j+1}",
+                        SeatTypeId = 3, //Hiện tại set cứng, PHẢI LẬP LUẬN ĐỂ SET SỐ LƯỢNG GHẾ THEO SeatType
+                        CompartmentId = compartment.Id,
+                    };
+                    await _seatRepository.Add(seat);
+                }
             }
             
-
             await _unitOfWork.SaveChangesAsync();
         }
 
