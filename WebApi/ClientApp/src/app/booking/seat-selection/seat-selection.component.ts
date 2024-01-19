048c7c4b0197b0c1fe31ab6d5fcb7c7623fa4d48
Huy Trần <nhokpio95@gmail.com>
2024-01-19 12:29:25 +0700

    Tạm hoàn thành html trang chọn ghế

--- WebApi/ClientApp/src/app/booking/seat-selection/seat-selection.component.ts
+++ WebApi/ClientApp/src/app/booking/seat-selection/seat-selection.component.ts
@@ -8,12 +8,39 @@ import { BookingService } from '../booking.service'
 })
 export class SeatSelectionComponent implements OnInit {
 
+  seatRows: number[][] = [];
   constructor(private bookingService: BookingService) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.bookingService.currentStep = 2;
     }, 0);
+    this.seatRows = this.generateSeatRows();
+  }
+
+  generateSeatRows(): number[][] {
+    const seatRows: number[][] = [];
+    const rows = 4;
+    const cols = 8;
+    const half = 32;
+
+    // Xử lý cho nửa đầu của các ghế
+    for (let row = 0; row < rows; row++) {
+      seatRows[row] = [];
+      for (let col = 0; col < cols; col++) {
+        seatRows[row][col] = row + 1 + col * rows;
+      }
+    }
+
+    // Xử lý cho nửa sau của các ghế
+    for (let row = 0; row < rows; row++) {
+      seatRows[row + rows] = []; // Bắt đầu từ hàng thứ 5 (index 4)
+      for (let col = 0; col < cols; col++) {
+        seatRows[row + rows][col] = half + row + 1 + col * rows;
+      }
+    }
+
+    return seatRows;
   }
 
 }
