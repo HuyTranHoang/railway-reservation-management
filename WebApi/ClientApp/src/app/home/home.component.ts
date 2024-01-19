d61c9920e6c083c31c76ceb2afb0813a124c515f
Huy Trần <nhokpio95@gmail.com>
2024-01-20 00:09:43 +0700

    Fix home page

--- WebApi/ClientApp/src/app/home/home.component.ts
+++ WebApi/ClientApp/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { DOCUMENT } from '@angular/common'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', '../booking/departure/departure.shared.scss']
 })
 export class HomeComponent implements AfterViewInit {
 
