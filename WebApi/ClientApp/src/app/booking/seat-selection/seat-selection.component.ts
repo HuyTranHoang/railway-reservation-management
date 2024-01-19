9288b2a7d2268665b5d7e8d007428e182ca932b2
Huy Trần <nhokpio95@gmail.com>
2024-01-19 11:27:34 +0700

    Thêm seat selection và naviagte cho booking

--- WebApi/ClientApp/src/app/booking/seat-selection/seat-selection.component.ts
+++ WebApi/ClientApp/src/app/booking/seat-selection/seat-selection.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core'
 import { BookingService } from '../booking.service'
 
 @Component({
-  selector: 'app-passengers',
-  templateUrl: './passengers.component.html',
-  styleUrls: ['./passengers.component.scss']
+  selector: 'app-seat-selection',
+  templateUrl: './seat-selection.component.html',
+  styleUrls: ['./seat-selection.component.scss']
 })
-export class PassengersComponent implements OnInit{
+export class SeatSelectionComponent implements OnInit {
 
   constructor(private bookingService: BookingService) {}
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.bookingService.currentStep = 3;
+      this.bookingService.currentStep = 2;
     }, 0);
   }
 
