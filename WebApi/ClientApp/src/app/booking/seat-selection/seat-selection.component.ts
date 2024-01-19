968439f464d7bdc2dd0a6d98c3b8a6ee63e65ce2
Huy Trần <nhokpio95@gmail.com>
2024-01-19 12:45:26 +0700

    Update seat selection có thể đổi màu ghế đã chọn.

--- WebApi/ClientApp/src/app/booking/seat-selection/seat-selection.component.ts
+++ WebApi/ClientApp/src/app/booking/seat-selection/seat-selection.component.ts
@@ -9,38 +9,44 @@ import { BookingService } from '../booking.service'
 export class SeatSelectionComponent implements OnInit {
 
   seatRows: number[][] = [];
+  seatStatus: boolean[][] = [];
   constructor(private bookingService: BookingService) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.bookingService.currentStep = 2;
     }, 0);
-    this.seatRows = this.generateSeatRows();
+    this.generateSeatRows();
   }
 
-  generateSeatRows(): number[][] {
-    const seatRows: number[][] = [];
+  generateSeatRows(): void {
     const rows = 4;
     const cols = 8;
     const half = 32;
 
     // Xử lý cho nửa đầu của các ghế
     for (let row = 0; row < rows; row++) {
-      seatRows[row] = [];
+      this.seatRows[row] = [];
+      this.seatStatus[row] = []; // Khởi tạo mảng trạng thái cho hàng
       for (let col = 0; col < cols; col++) {
-        seatRows[row][col] = row + 1 + col * rows;
+        this.seatRows[row][col] = row + 1 + col * rows;
+        this.seatStatus[row][col] = false; // Mặc định là ghế chưa đặt
       }
     }
 
     // Xử lý cho nửa sau của các ghế
     for (let row = 0; row < rows; row++) {
-      seatRows[row + rows] = []; // Bắt đầu từ hàng thứ 5 (index 4)
+      this.seatRows[row + rows] = []; // Bắt đầu từ hàng thứ 5 (index 4)
+      this.seatStatus[row + rows] = []; // Khởi tạo mảng trạng thái cho hàng
       for (let col = 0; col < cols; col++) {
-        seatRows[row + rows][col] = half + row + 1 + col * rows;
+        this.seatRows[row + rows][col] = half + row + 1 + col * rows;
+        this.seatStatus[row + rows][col] = false; // Mặc định là ghế chưa đặt
       }
     }
+  }
 
-    return seatRows;
+  toggleSeat(row: number, col: number): void {
+    this.seatStatus[row][col] = !this.seatStatus[row][col];
   }
 
 }
