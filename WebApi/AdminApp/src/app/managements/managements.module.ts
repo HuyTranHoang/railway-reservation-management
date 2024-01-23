249a6732ff3805c28872db04f39ec01af86b6540
Huy Trần <nhokpio95@gmail.com>
2024-01-23 07:47:53 +0700

    Little dashboard in admin

--- WebApi/AdminApp/src/app/managements/managements.module.ts
+++ WebApi/AdminApp/src/app/managements/managements.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ManagementsComponent} from './managements.component';
 import {ThemeModule} from '../@theme/theme.module';
-import {NbIconLibraries, NbInputModule, NbLayoutModule, NbMenuModule} from '@nebular/theme';
+import {NbIconLibraries, NbInputModule, NbMenuModule} from '@nebular/theme';
 import {DashboardModule} from '../pages/dashboard/dashboard.module';
 import {ECommerceModule} from '../pages/e-commerce/e-commerce.module';
 import {MiscellaneousModule} from '../pages/miscellaneous/miscellaneous.module';
 import {ManagementsRoutingModule} from './managements-routing.module';
-import {DashboardComponent} from './dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http';
-import {PagesRoutingModule} from '../pages/pages-routing.module';
+import {SharedModule} from './shared/shared.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -27,6 +27,8 @@ import {PagesRoutingModule} from '../pages/pages-routing.module';
     ECommerceModule,
     MiscellaneousModule,
     NbInputModule,
+
+    SharedModule,
   ],
 })
 export class ManagementsModule {
