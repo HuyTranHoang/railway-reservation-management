afb6e5e820bd13b9f48e3369eabe8c86a2a8ec96
Huy Trần <nhokpio95@gmail.com>
2024-01-25 18:30:19 +0700

    reformat management booking

--- WebApi/ClientApp/src/app/manage-booking/management-booking/management-booking.component.ts
+++ WebApi/ClientApp/src/app/manage-booking/management-booking/management-booking.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ManageBookingService } from '../manage-booking.service';
-import {  Router } from '@angular/router';
+import { Component } from '@angular/core'
+import { ManageBookingService } from '../manage-booking.service'
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -9,39 +9,40 @@ import {  Router } from '@angular/router';
   styleUrls: ['./management-booking.component.scss']
 })
 export class ManagementBookingComponent {
-  code: string = '';
-  email: string = '';
-  wrong : string = "";
+  code: string = ''
+  email: string = ''
+  wrong: string = ''
 
-  
-  constructor(private manageService : ManageBookingService,
-    private router: Router,
-    
-    ) {
+
+  constructor(private manageService: ManageBookingService,
+              private router: Router
+  ) {
   }
+
   onSubmit() {
-    this.manageService.getLookUpByEmailAndCode(this.code , this.email).subscribe({
-      next: (respon) => {
-        console.log(respon);
-        this.router.navigateByUrl('management/ticket');
-        this.router.navigate(['management/ticket'], { queryParams: {
-           email: this.email,
-           code: this.code,
-           passengerName : respon.passengerName,
-           trainName : respon.trainName,
-           carriageName : respon.carriageName,
-           seatName : respon.seatName,
-           scheduleName : respon.scheduleName,
-           price : respon.price,
-           scheduleId : respon.scheduleId,
-          } });
+    this.manageService.getLookUpByEmailAndCode(this.code, this.email).subscribe({
+      next: (res) => {
+        this.router.navigate(['management/ticket'], {
+          queryParams: {
+            email: this.email,
+            code: this.code,
+            passengerName: res.passengerName,
+            trainName: res.trainName,
+            carriageName: res.carriageName,
+            seatName: res.seatName,
+            scheduleName: res.scheduleName,
+            price: res.price,
+            scheduleId: res.scheduleId
+          }
+        })
       },
+
       error: (error: any) => {
-        console.log(error);
-        this.router.navigateByUrl('management');
-        this.wrong = "Failed wrong Email or Code please try again !"
+        console.log(error)
+        this.router.navigateByUrl('management')
+        this.wrong = 'Failed wrong Email or Code please try again !'
       }
-    });               
+    })
   }
 
 }
