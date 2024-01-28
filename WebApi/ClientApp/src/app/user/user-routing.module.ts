074827e524b025f6ed81d92b67848a08dd256d13
Huy Trần <nhokpio95@gmail.com>
2024-01-28 15:26:14 +0700

    Thêm guard cho user routing

--- WebApi/ClientApp/src/app/user/user-routing.module.ts
+++ WebApi/ClientApp/src/app/user/user-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserBookingHistoryComponent } from './user-booking-history/user-booking-history.component';
+import { AuthorizationGuard } from '../core/guards/authorization.guard'
 
 const routes: Routes = [
   {
     path: 'profile',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthorizationGuard],
     component: UserProfileComponent,
   },
   {
     path: 'booking',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthorizationGuard],
     component : UserBookingHistoryComponent,
   }
 ];
